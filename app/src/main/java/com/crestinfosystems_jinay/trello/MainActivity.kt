f164268c8b20a6d3669fbf37c6ddbda363852c22
mr-j-shah <shahjinay02@gmail.com>
2024-03-15 17:33:51 +0530

    Add Notification based on Sub

--- app/src/main/java/com/crestinfosystems_jinay/trello/MainActivity.kt
+++ app/src/main/java/com/crestinfosystems_jinay/trello/MainActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.crestinfosystems_jinay.trello.Screens_Activity.HomePage
 import com.crestinfosystems_jinay.trello.databinding.ActivityMainBinding
 import com.crestinfosystems_jinay.trello.splash_pages.splash_screen
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,18 +31,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding?.root)
-        Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            val user = mAuth.currentUser
-            if (user != null) {
-                val intent = Intent(this, HomePage::class.java)
-                startActivity(intent)
-                this.finish()
-            } else {
-                val intent: Intent = Intent(this, splash_screen::class.java)
-                startActivity(intent)
-                finish() // Op
+        Handler(Looper.getMainLooper()).postDelayed(
+            Runnable {
+                val user = mAuth.currentUser
+                if (user != null) {
+                    val intent = Intent(this, HomePage::class.java)
+                    startActivity(intent)
+                    this.finish()
+                } else {
+                    val intent: Intent = Intent(this, splash_screen::class.java)
+                    startActivity(intent)
+                    finish() // Op
+                }
+            },
+            3000
+        )
+        Firebase.messaging.subscribeToTopic(Firebase.auth.currentUser!!.email.toString())
+            .addOnCompleteListener { task ->
+                var msg = "Subscribed"
+                if (!task.isSuccessful) {
+                    msg = "Subscribe failed"
+                }
+                Log.d("TAG", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
             }
-        }, 3000)
         binding?.appVersion?.setText("v " + versionName);
     }
 }
